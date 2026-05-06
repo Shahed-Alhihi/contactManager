7357a6c64304621220250ee70e62927ee387731b
Shahed-Alhihi <shahedmousa39@gmail.com>
2026-05-07 01:22:56 +0300

    contact manager lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,205 +1,39 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <section id="center">
-//         <div className="hero">
-//           <img src={heroImg} className="base" width="170" height="179" alt="" />
-//           <img src={reactLogo} className="framework" alt="React logo" />
-//           <img src={viteLogo} className="vite" alt="Vite logo" />
-//         </div>
-//         <div>
-//           <h1>Get started</h1>
-//           <p>
-//             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-//           </p>
-//         </div>
-//         <button
-//           type="button"
-//           className="counter"
-//           onClick={() => setCount((count) => count + 1)}
-//         >
-//           Count is {count}
-//         </button>
-//       </section>
-
-//       <div className="ticks"></div>
-
-//       <section id="next-steps">
-//         <div id="docs">
-//           <svg className="icon" role="presentation" aria-hidden="true">
-//             <use href="/icons.svg#documentation-icon"></use>
-//           </svg>
-//           <h2>Documentation</h2>
-//           <p>Your questions, answered</p>
-//           <ul>
-//             <li>
-//               <a href="https://vite.dev/" target="_blank">
-//                 <img className="logo" src={viteLogo} alt="" />
-//                 Explore Vite
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://react.dev/" target="_blank">
-//                 <img className="button-icon" src={reactLogo} alt="" />
-//                 Learn more
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div id="social">
-//           <svg className="icon" role="presentation" aria-hidden="true">
-//             <use href="/icons.svg#social-icon"></use>
-//           </svg>
-//           <h2>Connect with us</h2>
-//           <p>Join the Vite community</p>
-//           <ul>
-//             <li>
-//               <a href="https://github.com/vitejs/vite" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#github-icon"></use>
-//                 </svg>
-//                 GitHub
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://chat.vite.dev/" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#discord-icon"></use>
-//                 </svg>
-//                 Discord
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://x.com/vite_js" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#x-icon"></use>
-//                 </svg>
-//                 X.com
-//               </a>
-//             </li>
-//             <li>
-//               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-//                 <svg
-//                   className="button-icon"
-//                   role="presentation"
-//                   aria-hidden="true"
-//                 >
-//                   <use href="/icons.svg#bluesky-icon"></use>
-//                 </svg>
-//                 Bluesky
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </section>
-
-//       <div className="ticks"></div>
-//       <section id="spacer"></section>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { useState,useEffect } from "react";
-import PeopleForm from "./PeopleForm";
-import PeopleList from "./PeopleList";
-import Digimon from "./Digimon"
+import { useState } from 'react'
+import './App.css'
+import ContactForm from'./components/ContactForm'
+import List from './components/List'
 
 function App() {
-  const [people, setPeople] = useState([]);
+  const [contacts, setContact] = useState([])
 
-  const addPerson = (newPerson) => {
-    setPeople([...people, newPerson]);
-  }
+  const addContact=(newContact) =>{
+    setContact([...contacts, newContact]);
 
-  //  removeperson func
+  };
 
-
-  // ✅ useEffect runs Only on mount (first render) the initial rendering of the component 
-  // useEffect(() => {
-  //   console.log("I run on the first Render only (mounting)");
-
-  // }, [])
-
-  // ✅ useEffect runs every time the people array changes
-  useEffect(() => {
-    // console.log(people);
-
-    if (people.length > 0) {
-      console.log(`✅ New person added: ${people[people.length - 1].name}`);
-      console.log(`Total people now: ${people.length}`);
-    }
-  }, [people]);
-
-  
-  // ✅ useEffect runs Runs when the component unmounts
-  // ✔ Good for removing timers, listeners, sockets, intervals
-  useEffect(() => {
-    return () => {
-      console.log("🧹 cleanup: runs on UNMOUNT");
-    };
-  });
+  const deleteContact=(contactID)=>{
+    setContact(contacts.filter((contacts,index)=> index!==contactID));
+  };
 
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <>
+    <div className='app-container'>
+      <h1> Contact Manager App </h1>
+   
+    
+     <ContactForm addContact={addContact}/>
+      <List contacts={contacts} deleteContact={deleteContact }/>
 
-      <PeopleForm addPerson={addPerson} />
-      <PeopleList people={people} />
+    
+ </div>
+        
+         
+       
 
-      {/* axios */}
-      <Digimon/>
-    </div>
+
+    </>
   );
 }
 
-export default App;
+export default App
--- src/Greeting.jsx
+++
@@ -1,7 +0,0 @@
-function Greeting({username}) {
-// function Greeting(props) {
-    return <h2>Hello, {username} 👋</h2>;
-  }
-  
-  export default Greeting;
-  
